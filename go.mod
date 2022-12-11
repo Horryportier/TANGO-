b72b031c278eaead0c02cd1200c16e215aa4b827
Horryportier <kowalskis354@gmail.com>
2022-12-11 21:18:34 +0100

    2 commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module src/tango
+
+go 1.18
+
+require github.com/Horryportier/go-jisho v0.0.0-20221211194917-401100e3a975
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	jisho "github.com/Horryportier/go-jisho"
+)
+
+func main() {
+        word, err := jisho.Search("bird")
+        if err != nil {
+                log.Fatal(err)
+        }
+        fmt.Printf("data: %v \n", word)
+
+        fmt.Printf("JAPANES: %v\n", word.TransJapan(0))
+        fmt.Printf("ENG: %v\n", word.EngDefinition(0))
+}
